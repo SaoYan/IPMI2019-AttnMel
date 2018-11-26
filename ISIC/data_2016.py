a7618c646598da622084dce6e19a7cd0711415e1
saoyan <yanyiqinwpu@gmail.com>
2018-11-26 12:24:59 -0800

    update dataloader: load segmentation

--- ISIC/data_2016.py
+++ ISIC/data_2016.py
@@ -13,34 +13,53 @@ def preprocess_data(root_dir):
     # training data
     benign    = glob.glob(os.path.join(root_dir, 'Train', 'benign', '*.jpg')); benign.sort()
     malignant = glob.glob(os.path.join(root_dir, 'Train', 'malignant', '*.jpg')); malignant.sort()
+    benign_seg    = glob.glob(os.path.join(root_dir, 'Train_Seg', 'benign', '*.png')); benign_seg.sort()
+    malignant_seg = glob.glob(os.path.join(root_dir, 'Train_Seg', 'malignant', '*.png')); malignant_seg.sort()
     with open('train.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for filename in benign:
-            writer.writerow([filename] + ['0'])
-        for filename in malignant:
-            writer.writerow([filename] + ['1'])
+        for k in range(len(benign)):
+            filename = benign[k]
+            filename_seg = benign_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(malignant)):
+            filename = malignant[k]
+            filename_seg = malignant_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
     # training data oversample
     benign    = glob.glob(os.path.join(root_dir, 'Train', 'benign', '*.jpg')); benign.sort()
     malignant = glob.glob(os.path.join(root_dir, 'Train', 'malignant', '*.jpg')); malignant.sort()
+    benign_seg    = glob.glob(os.path.join(root_dir, 'Train_Seg', 'benign', '*.png')); benign_seg.sort()
+    malignant_seg = glob.glob(os.path.join(root_dir, 'Train_Seg', 'malignant', '*.png')); malignant_seg.sort()
     with open('train_oversample.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for filename in benign:
-            writer.writerow([filename] + ['0'])
+        for k in range(len(benign)):
+            filename = benign[k]
+            filename_seg = benign_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
         for i in range(4):
-            for filename in malignant:
-                writer.writerow([filename] + ['1'])
+            for k in range(len(malignant)):
+                filename = malignant[k]
+                filename_seg = malignant_seg[k]
+                writer.writerow([filename] + [filename_seg] + ['1'])
     # test data
     benign    = glob.glob(os.path.join(root_dir, 'Test', 'benign', '*.jpg')); benign.sort()
     malignant = glob.glob(os.path.join(root_dir, 'Test', 'malignant', '*.jpg')); malignant.sort()
+    benign_seg    = glob.glob(os.path.join(root_dir, 'Test_Seg', 'benign', '*.png')); benign_seg.sort()
+    malignant_seg = glob.glob(os.path.join(root_dir, 'Test_Seg', 'malignant', '*.png')); malignant_seg.sort()
     with open('test.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for filename in benign:
-            writer.writerow([filename] + ['0'])
-        for filename in malignant:
-            writer.writerow([filename] + ['1'])
+        for k in range(len(benign)):
+            filename = benign[k]
+            filename_seg = benign_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(malignant)):
+            filename = malignant[k]
+            filename_seg = malignant_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
 
 class ISIC(udata.Dataset):
-    def __init__(self, csv_file, shuffle=True, rotate=True, transform=None):
+    def __init__(self, csv_file, shuffle=True, rotate=True, transform=None, transform_seg=None):
+        assert not (transform is None) ^ (transform_seg is None), 'is transform is (not) None, transform_seg must also be (not) None!'
         file = open(csv_file, newline='')
         reader = csv.reader(file, delimiter=',')
         self.pairs = [row for row in reader]
@@ -48,12 +67,14 @@ class ISIC(udata.Dataset):
             random.shuffle(self.pairs)
         self.rotate = rotate
         self.transform = transform
+        self.transform_seg = transform_seg
     def __len__(self):
         return len(self.pairs)
     def  __getitem__(self, idx):
         pair = self.pairs[idx]
         image = Image.open(pair[0])
-        label = int(pair[1])
+        image_seg = Image.open(pair[1])
+        label = int(pair[2])
         # center crop
         width, height = image.size
         new_size = 0.8 * min(width, height)
@@ -62,10 +83,13 @@ class ISIC(udata.Dataset):
         right = (width + new_size)/2
         bottom = (height + new_size)/2
         image = image.crop((left, top, right, bottom))
+        image_seg = image_seg.crop((left, top, right, bottom))
         # rotate
         if self.rotate:
             idx = random.randint(0,3)
             image = image.rotate(idx*90)
+            image_seg = image_seg.rotate(idx*90)
         if self.transform:
             image = self.transform(image)
+            image_seg = self.transform_seg(image)
         return image, label
--- ISIC/data_2017.py
+++ ISIC/data_2017.py
@@ -1,6 +1,7 @@
 import random
 import csv
 import os
+import h5py
 import os.path
 from PIL import Image
 import glob
@@ -14,42 +15,70 @@ def preprocess_data(root_dir):
     melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
     nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
     sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    melanoma_seg = glob.glob(os.path.join(root_dir, 'Train_Seg', 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, 'Train_Seg', 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, 'Train_Seg', 'seborrheic_keratosis', '*.png')); sk_seg.sort()
     with open('train.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for filename in melanoma:
-            writer.writerow([filename] + ['1'])
-        for filename in nevus:
-            writer.writerow([filename] + ['0'])
-        for filename in sk:
-            writer.writerow([filename] + ['0'])
+        for k in range(len(melanoma)):
+            filename = melanoma[k]
+            filename_seg = melanoma_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
     # training data oversample
     melanoma = glob.glob(os.path.join(root_dir, 'Train', 'melanoma', '*.jpg')); melanoma.sort()
     nevus    = glob.glob(os.path.join(root_dir, 'Train', 'nevus', '*.jpg')); nevus.sort()
     sk       = glob.glob(os.path.join(root_dir, 'Train', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    melanoma_seg = glob.glob(os.path.join(root_dir, 'Train_Seg', 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, 'Train_Seg', 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, 'Train_Seg', 'seborrheic_keratosis', '*.png')); sk_seg.sort()
     with open('train_oversample.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for i in range(4):
-            for filename in melanoma:
-                writer.writerow([filename] + ['1'])
-        for filename in nevus:
-            writer.writerow([filename] + ['0'])
-        for filename in sk:
-            writer.writerow([filename] + ['0'])
+            for k in range(len(melanoma)):
+                filename = melanoma[k]
+                filename_seg = melanoma_seg[k]
+                writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
     # test data
     melanoma = glob.glob(os.path.join(root_dir, 'Test', 'melanoma', '*.jpg')); melanoma.sort()
     nevus    = glob.glob(os.path.join(root_dir, 'Test', 'nevus', '*.jpg')); nevus.sort()
     sk       = glob.glob(os.path.join(root_dir, 'Test', 'seborrheic_keratosis', '*.jpg')); sk.sort()
+    melanoma_seg = glob.glob(os.path.join(root_dir, 'Test_Seg', 'melanoma', '*.png')); melanoma_seg.sort()
+    nevus_seg    = glob.glob(os.path.join(root_dir, 'Test_Seg', 'nevus', '*.png')); nevus_seg.sort()
+    sk_seg       = glob.glob(os.path.join(root_dir, 'Test_Seg', 'seborrheic_keratosis', '*.png')); sk_seg.sort()
     with open('test.csv', 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        for filename in melanoma:
-            writer.writerow([filename] + ['1'])
-        for filename in nevus:
-            writer.writerow([filename] + ['0'])
-        for filename in sk:
-            writer.writerow([filename] + ['0'])
+        for k in range(len(melanoma)):
+            filename = melanoma[k]
+            filename_seg = melanoma_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['1'])
+        for k in range(len(nevus)):
+            filename = nevus[k]
+            filename_seg = nevus_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
+        for k in range(len(sk)):
+            filename = sk[k]
+            filename_seg = sk_seg[k]
+            writer.writerow([filename] + [filename_seg] + ['0'])
 
-class ISIC2017(udata.Dataset):
-    def __init__(self, csv_file, shuffle=True, rotate=True, transform=None):
+class ISIC(udata.Dataset):
+    def __init__(self, csv_file, shuffle=True, rotate=True, transform=None, transform_seg=None):
+        assert not (transform is None) ^ (transform_seg is None), 'is transform is (not) None, transform_seg must also be (not) None!'
         file = open(csv_file, newline='')
         reader = csv.reader(file, delimiter=',')
         self.pairs = [row for row in reader]
@@ -57,12 +86,14 @@ class ISIC2017(udata.Dataset):
             random.shuffle(self.pairs)
         self.rotate = rotate
         self.transform = transform
+        self.transform_seg = transform_seg
     def __len__(self):
         return len(self.pairs)
     def  __getitem__(self, idx):
         pair = self.pairs[idx]
         image = Image.open(pair[0])
-        label = int(pair[1])
+        image_seg = Image.open(pair[1])
+        label = int(pair[2])
         # center crop
         width, height = image.size
         new_size = 0.8 * min(width, height)
@@ -71,10 +102,13 @@ class ISIC2017(udata.Dataset):
         right = (width + new_size)/2
         bottom = (height + new_size)/2
         image = image.crop((left, top, right, bottom))
+        image_seg = image_seg.crop((left, top, right, bottom))
         # rotate
         if self.rotate:
             idx = random.randint(0,3)
             image = image.rotate(idx*90)
+            image_seg = image_seg.rotate(idx*90)
         if self.transform:
             image = self.transform(image)
+            image_seg = self.transform_seg(image)
         return image, label
